910100d1a4df6c05f8e591fa9dc7cad1344583f6
benvein <kokobence2006@gmail.com>
2024-12-10 13:39:36 +0100

    xd

--- decdogaFeladatok/xo.js
+++ decdogaFeladatok/xo.js
@@ -3,14 +3,11 @@ import input from './input.js'
 let str = await input("text: ");
 
 function XO(str) {
-    // Convert the string to lowercase
     const lowerStr = str.toLowerCase();
     
-    // Count occurrences of 'x' and 'o'
     const xCount = (lowerStr.match(/x/g) || []).length;
     const oCount = (lowerStr.match(/o/g) || []).length;
     
-    // Return true if counts are equal, otherwise false
     return xCount === oCount;
 }
 
