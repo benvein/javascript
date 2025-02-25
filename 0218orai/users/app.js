98e6f9448fd1bb049a32c6742d7b3ca677d8acc7
benvein <kokobence2006@gmail.com>
2025-02-25 15:06:24 +0100

    xd

--- 0218orai/users/app.js
+++ 0218orai/users/app.js
@@ -1,48 +1,73 @@
 import express from 'express';
+import path from 'path';    
 import __dirname from "./util/rootpath.js";
+import * as fileHandler from "./util/filekezeles.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
-const users = [
-    { firstName: "Harry", lastName: "Potter" },
-    { firstName: "Ronald", lastName: "Bilius Weasley" },
-    { firstName: "Hermione", lastName: "Jean Granger" },
-    { firstName: "Draco", lastName: "Malfoy" },
-    { firstName: "Cedric", lastName: "Diggory" },
-    { firstName: "Luna", lastName: "Lovegood" },
-];
+const users = fileHandler.getData();
 
 app.get("/users", (req,res)=>{
-    res.send(users);
+    const users = fileHandler.getData();
+    res.json(users);
 });
 
 app.get("/users/:id", (req,res)=>{
-    const user = req.params.id;
-    res.json(users[user]);
+    const users = fileHandler.getData();
+    const id = req.params.id;
+    if(id<0 || id>=users.length){
+        return res.json({});
+    }
+    res.json(users[id]);
 });
 
 app.post("/users", (req,res)=>{
+    const users = fileHandler.getData();
     const {firstName, lastName} = req.body;
-    console.log(`name: ${firstName} ${lastName}`);
-    res.json(firstName, lastName);
+    const newUser = {firstName, lastName}
+    users.push(newUser);
+    fileHandler.saveData(users);
+    res.json(newUser);
 });
 
-app.post("/users/:id", (req,res)=>{
-    const user = req.params.id;
+app.put("/users/:id", (req,res)=>{
+    const users = fileHandler.getData();
+    const id = req.params.id;
+    if(id<0 || id>=users.length){
+        return res.json({message: "user not found"});
+    }
     const {firstName, lastName} = req.body;
-    res.json(users[user]);
+    users[id] = {firstName, lastName};
+    fileHandler.saveData(users);
+    res.json(users[id]);
 });
 
 app.patch("/users/:id", (req,res)=>{
-    const user = req.params.id;
+    const users = fileHandler.getData();
+    const id = req.params.id;
+    if(id<0 || id>=users.length){
+        return res.json({message: "user not found"});
+    }
     const {firstName, lastName} = req.body;
-    res.json(users[user]);
+    users[id] = {
+        firstName: firstName || users[id].firstName, 
+        lastName: lastName || users[id].lastName
+    };
+    fileHandler.saveData(users);
+    res.json(users[id]);
 });
 
 app.delete("/users/:id", (req,res)=>{
-    const user = req.params.id;
-    res.json(users[user]);
+    const users = fileHandler.getData();
+    const id = req.params.id;
+    if(id<0 || id>=users.length){
+        return res.json({message: "user not found"});
+    }
+    users.splice(id,1);
+    fileHandler.saveData(users);
+    res.json({message: "deelete susccccesful"});
 });
 
 
