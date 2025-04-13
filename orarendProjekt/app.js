90d03d76d1a2d359992ea30b6a4acbf0d5bed159
User <user@192.168.1.73>
2025-04-13 10:00:39 +0200

    projekt1

--- orarendProjekt/app.js
+++ orarendProjekt/app.js
@@ -16,7 +16,7 @@ app.get("timetable/:id", async (req, res) => {
         req.params.id
     ]);
     if(!timetable) {
-        return res.status(404).json({message: "record not found"});
+        return res.status(404).sendFile("./views/404.html", {root : __dirname});
     }
     res.status(200).json(record);
 });
@@ -24,7 +24,7 @@ app.get("timetable/:id", async (req, res) => {
 app.post("/timetable", async (req,res) => {
     const {day, subject, period} = req.body;
     if(!day, !subject, !period){
-        return res.status(404).json({message: "invalid data"});
+        return res.status(404).sendFile("./views/404.html", {root : __dirname});
     }
     const result = await dbRun(`INSERT INTO timetable (day, subject, period) VALUES (?, ?, ?);`, [day, subject, period]);
     res.status(201).json({id: result.lastID, day, subject, period});
@@ -34,11 +34,11 @@ app.put("/timetable/:id", async (req,res) => {
     const id = req.params.id;
     const record = await dbGet("SELECT * FROM timetable WHERE id = ?;", [id]);
     if(!record){
-        return res.status(404).json({message: "record not found"});
+        return res.status(404).sendFile("./views/404.html", {root : __dirname});
     }
     const {day, subject, period} = req.body;
     if(!day, !subject, !period){
-        return res.status(404).json({message: "invalid data"});
+        return res.status(404).sendFile("./views/404.html", {root : __dirname});
     }
     dbRun("UPDATE timetable SET day = ?, subject = ?, period = ? WHERE id = ?;", [day, subject, period, id]);
     res.status(200).json({id, day, subject, period});
@@ -48,7 +48,7 @@ app.delete("/timetable/:id", async (req,res) => {
     const id = req.params.id;
     const record = await dbGet("SELECT * FROM timetable WHERE id = ?;", [id]);
     if(!record){
-        return res.status(404).json({message: "record not found"});
+        return res.status(404).sendFile("./views/404.html", {root : __dirname});
     }
     dbRun("DELETE FROM timetable WHERE id = ?;", [id]);
     res.status(200).json({message: "deleted"});
