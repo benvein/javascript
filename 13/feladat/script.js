14b8a82c89c64fd7ca097fafe50254b6d5294e10
benvein <kokobence2006@gmail.com>
2025-09-05 11:29:38 +0200

    nem fogom birni ezt az evet

--- 13/feladat/script.js
+++ 13/feladat/script.js
@@ -1,21 +1,29 @@
-document.getElementById("dataForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+document.getElementById('dataForm').addEventListener('submit', function (e) {
+    e.preventDefault();
 });
 
-let name = document.getElementById("name").value;
-let postalCode = document.getElementById("postalcode").value;
-let city = document.getElementById("city").value;
-let street = document.getElementById("street").value;
-let housenum = document.getElementById("housenum").value;
+function saveData() {
+    let name = document.getElementById('name').value;
+    let postalCode = parseInt(document.getElementById('postalcode').value);
+    let city = document.getElementById('city').value;
+    let street = document.getElementById('street').value;
+    let housenum = document.getElementById('housenum').value;
 
-const data = {
-  name,
-  postalCode,
-  city,
-  street,
-  housenum,
-};
+    var data = {
+        name,
+        postalCode,
+        city,
+        street,
+        housenum,
+    };
 
-localStorage.setItem("data", JSON.stringify({ data }));
+    localStorage.setItem('data', JSON.stringify({ data }));
+    console.log('data saved');
+}
 
-let savedData = JSON.parse(localStorage.getItem("data"));
+var savedData = JSON.parse(localStorage.getItem('data'));
+function showStuff(savedData) {
+    return (document.getElementById(
+        'showData',
+    ).innerHTML = `nev: ${savedData.name}<br>irszam: ${savedData.postalCode}<br>varos: ${savedData.city}<br>kozterulet: ${savedData.city}<br>hsz:: ${savedData.housenum}<br>`);
+}
